cd6fd9f6e6159069f66146567e9f1902b76aaefb
Rebecca Hong <Rebeccahongsf@gmail.com>
2019-11-25 19:28:37 -0800

    Add proptypes

--- src/Movie.js
+++ src/Movie.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Movie extends Component {
+  static propTypes = {
+    movie: PropTypes.shape({
+      title: PropTypes.string.isRequired
+    }),
+    desc: PropTypes.string
+  }
+  
+  static defaultProps = {
+    desc: 'Description not available.'
+  }
+  
   render() {
     return(
       <div>
-        {this.props.movie.title}
+        <h1>{this.props.movie.title}</h1>
+        <p>{this.props.desc}</p>
       </div>
     )
   }
