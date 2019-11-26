eb40fe7765e581105543493c61e5adb4b93c18e9
Rebecca Hong <Rebeccahongsf@gmail.com>
2019-11-25 21:31:18 -0800

    Remove Movies array

--- src/App.js
+++ src/App.js
@@ -5,22 +5,6 @@ import Movie from './Movie';
 import logo from './logo.svg';
 import './App.css';
 
-const movies = [{
-    id: 1,
-    title: 'Star Wars',
-    desc: 'Nullam dictum felis eu pede mollis pretium.'
-  }, {
-    id: 2,
-    title: 'Spider Man'
-  }, {
-    id: 3,
-    title: '36th Chamber of Shaolin'
-  }, {
-    id: 4,
-    title: '5 Deadly Venoms'
-  }
-];
-
 class App extends Component {  
   state = {
     movies: []
@@ -46,7 +30,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {movies.map((movie) => (
+        {this.state.movies.map((movie) => (
           <Movie 
             key={movie.id}
             movie={movie}
