84fde761af7bd4cd8f8d5faf53165e5b177bd7d3
Rebecca Hong <Rebeccahongsf@gmail.com>
2019-11-28 07:44:16 -0800

    Add Poster Path; Add react-router-dom

--- src/Movie.js
+++ src/Movie.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Component } from 'react';
 import PropTypes from 'prop-types';
 
+const POSTER_PATH = 'https://image.tmdb.org/t/p/w154';
 
 // Functional Stateless Components
 // This should be used when you are not using lifecycle methods or state refs.
 // ES6 Object Destruction: Because of object destructuring, we can use {movie} inplace of props. 
 const Movie = ({movie}) => (
   <div>
-    <h3>{movie.title}</h3>
+    <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
   </div>
 );
 
