313e197af15e1c0196e8a6a42c66bf72d6ada642
Rebecca Hong <Rebeccahongsf@gmail.com>
2019-11-30 08:24:56 -0800

    Updates

---
+++ precache-manifest.bbf744dd3db8716049bde69650c64861.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac11d19c9dced582cc0c7c9bcd16a6f4",
+    "url": "/react-moviedb/index.html"
+  },
+  {
+    "revision": "448c4c104dd7a9f0bea8",
+    "url": "/react-moviedb/static/css/main.a3310698.chunk.css"
+  },
+  {
+    "revision": "6dcaa6157813de4b748a",
+    "url": "/react-moviedb/static/js/2.fe1ede22.chunk.js"
+  },
+  {
+    "revision": "448c4c104dd7a9f0bea8",
+    "url": "/react-moviedb/static/js/main.d90c9591.chunk.js"
+  },
+  {
+    "revision": "96df98f585a36d256e23",
+    "url": "/react-moviedb/static/js/runtime-main.04571f11.js"
+  },
+  {
+    "revision": "ae29b6c622c79700076fba6c09460280",
+    "url": "/react-moviedb/static/media/logo.ae29b6c6.svg"
+  }
+]);
